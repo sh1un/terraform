4f31e3f39ece53054bbb4a41907317cab8d5e87a
sh1un <a0923183408@gmail.com>
2024-06-08 17:00:50 +0800

    feat: add API Gateway configuration for practice API

---
+++ practice/API Gateway with Lambda/api_gateway.tf
@@ -0,0 +1,47 @@
+resource "aws_api_gateway_rest_api" "my_api" {
+  name        = "my-api"
+  description = "This is my API for practice"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+
+
+}
+
+resource "aws_api_gateway_resource" "root" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  parent_id   = aws_api_gateway_rest_api.my_api.root.rest_api_id
+  path_part   = "mypath"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.root.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_resource.root.id
+  resource_id             = aws_api_gateway_resource.root.id
+  http_method             = aws_api_gateway_method.proxy.http_method
+  integration_http_method = "POST"
+  type                    = "MOCK"
+}
+
+resource "aws_api_gateway_method_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = aws_api_gateway_method_response.proxy.status_code
+
+  depends_on = [aws_api_gateway_method.proxy, aws_api_gateway_integration.lambda_integration]
+}
