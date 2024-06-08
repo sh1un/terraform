ff684716c606e5d0f0d992a4a758ed50e805074a
sh1un <a0923183408@gmail.com>
2024-06-09 04:23:27 +0800

    feat: add CORS headers to API Gateway responses

--- practice/API Gateway with Lambda/api_gateway.tf
+++ practice/API Gateway with Lambda/api_gateway.tf
@@ -36,6 +36,13 @@ resource "aws_api_gateway_method_response" "proxy" {
   resource_id = aws_api_gateway_resource.root.id
   http_method = aws_api_gateway_method.proxy.http_method
   status_code = "200"
+
+  # CORS
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
 }
 
 resource "aws_api_gateway_integration_response" "proxy" {
@@ -44,12 +51,20 @@ resource "aws_api_gateway_integration_response" "proxy" {
   http_method = aws_api_gateway_method.proxy.http_method
   status_code = aws_api_gateway_method_response.proxy.status_code
 
+  # CORS
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'*'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
   depends_on = [aws_api_gateway_method.proxy, aws_api_gateway_integration.lambda_integration]
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [aws_api_gateway_integration.lambda_integration]
+  depends_on = [aws_api_gateway_integration.lambda_integration, aws_api_gateway_integration_response.proxy]
 
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   stage_name  = "dev"
+  description = timestamp()
 }
