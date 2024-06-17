b838b8a837a59b27c4ba9aadaff242b4e2cd72fb
sh1un <a0923183408@gmail.com>
2024-06-17 23:22:50 +0800

    feat: add AWS API Gateway configuration for Lambda integration

---
+++ modules/api_gateway_lambda/api_gateway.tf
@@ -0,0 +1,81 @@
+resource "aws_api_gateway_rest_api" "my_api" {
+  name        = var.api_gateway_name
+  description = var.api_gateway_description
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+
+
+}
+
+resource "aws_api_gateway_resource" "root" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
+  path_part   = "mypath"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.root.id
+  http_method   = "POST"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.authorizer.id
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.my_api.id
+  resource_id             = aws_api_gateway_resource.root.id
+  http_method             = aws_api_gateway_method.proxy.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.html_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = "200"
+
+  # CORS
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = aws_api_gateway_method_response.proxy.status_code
+
+  # CORS
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'*'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [aws_api_gateway_method.proxy, aws_api_gateway_integration.lambda_integration]
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [aws_api_gateway_integration.lambda_integration, aws_api_gateway_integration_response.proxy]
+
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  stage_name  = "dev"
+  triggers = {
+    redeployment = timestamp()
+
+  }
+}
+
+resource "aws_api_gateway_authorizer" "authorizer" {
+  name          = "my-apigw-authorizer"
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  type          = "COGNITO_USER_POOLS"
+  provider_arns = [aws_cognito_user_pool.pool.arn]
+}
