8238385a4c30b267b2ae6b972f184dae2032cf72
sh1un <a0923183408@gmail.com>
2024-06-18 23:57:57 +0800

    feat: add IAM role and policies for Lambda function

---
+++ practice/api_gateway_cognito_lambda_dynamodb/iam.tf
@@ -0,0 +1,32 @@
+
+# Assume role policy for API Gateway
+data "aws_iam_policy_document" "AWSLambdaTrusyPolicy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+# IAM role for Lambda function
+resource "aws_iam_role" "terraform_function_role" {
+
+  name               = "terraform_function_role"
+  assume_role_policy = data.aws_iam_policy_document.AWSLambdaTrusyPolicy.json
+
+}
+
+resource "aws_iam_role_policy_attachment" "terraform_lambda_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.terraform_function_role.name
+
+}
+
+resource "aws_iam_role_policy" "lambda_dynamodb_policy" {
+  name   = "lambda_dynamodb_policy"
+  role   = aws_iam_role.terraform_function_role.name
+  policy = file("${path.module}/lambda_dynamodb_policy.json")
+}
