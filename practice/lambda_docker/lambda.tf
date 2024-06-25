db6bcf291d476e4f724bc551857e33a67894630a
sh1un <a0923183408@gmail.com>
2024-06-25 22:57:34 +0800

    feat: finish lambda docker

--- practice/lambda_docker/lambda.tf
+++ practice/lambda_docker/lambda.tf
@@ -1,42 +1,65 @@
 data "aws_ecr_authorization_token" "token" {
 }
 
+data "aws_caller_identity" "this" {}
+
+resource "random_string" "this" {
+  length  = 4
+  special = false
+  lower   = true
+  upper   = false
+}
+
 locals {
-  name        = "demo-lambda-docker-image"
-  source_path = abspath(path.module)
+  source_path                     = path.module # Path to folder containing application code
+  function_name_and_ecr_repo_name = "${var.environment}-${local.service}-from_terraform_module_function-${random_string.this.result}"
+  service                         = "demo_service"
+  path_include                    = ["**"]
+  path_exclude                    = ["**/__pycache__/**"]
+  files_include                   = setunion([for f in local.path_include : fileset(local.source_path, f)]...)
+  files_exclude                   = setunion([for f in local.path_exclude : fileset(local.source_path, f)]...)
+  files                           = sort(setsubtract(local.files_include, local.files_exclude))
+  dir_sha                         = sha1(join("", [for f in local.files : filesha1("${local.source_path}/${f}")]))
 }
 
 provider "docker" {
   registry_auth {
-    address  = "070576557102.dkr.ecr.${var.aws_region}.amazonaws.com" # rich-liu
+    address  = format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.this.account_id, var.aws_region)
     username = data.aws_ecr_authorization_token.token.user_name
     password = data.aws_ecr_authorization_token.token.password
   }
 }
 
 
-
-
-module "aws_lambda" {
+module "lambda_container_image" {
   source  = "terraform-aws-modules/lambda/aws"
   version = "7.7.0"
 
-  function_name  = "from_terraform_module_function"
+  function_name  = local.function_name_and_ecr_repo_name
   description    = "This is a demo, for lambda container image"
   create_package = false
-  package_type   = "Image"
-  image_uri      = module.docker_image.image_uri
   timeout        = 60
 
-  publish = true
+  ##################
+  # Container Image
+  ##################
+  package_type  = "Image"
+  architectures = ["x86_64"] # or ["arm64"]
+  image_uri     = module.docker_image.image_uri
+
+  publish = true # Whether to publish creation/change as new Lambda Function Version.
 
 
   environment_variables = {
-    "environment" = "poc",
+    "ENVIRONMENT" = var.environment,
+    "SERVICE"     = local.service
   }
 
   tags = {
-    "Terraform" = "true"
+    "Terraform"   = "true",
+    "Environment" = var.environment,
+    "Service"     = local.service
+
   }
 }
 
@@ -44,13 +67,32 @@ module "docker_image" {
   source  = "terraform-aws-modules/lambda/aws//modules/docker-build"
   version = "7.7.0"
 
-  create_ecr_repo = true
-  keep_remotely   = true
-  use_image_tag   = true
-  ecr_repo        = local.name
+  create_ecr_repo      = true
+  keep_remotely        = true
+  use_image_tag        = false
+  image_tag_mutability = "MUTABLE"
+  ecr_repo             = local.function_name_and_ecr_repo_name
+  ecr_repo_lifecycle_policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Keep only the last 10 images",
+        "selection" : {
+          "tagStatus" : "any",
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 10
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      }
+    ]
+  })
 
-  image_tag        = "latest"
-  docker_file_path = "./Dockerfile"
+  docker_file_path = "${local.source_path}/Dockerfile"
   source_path      = local.source_path
+  triggers = {
+    dir_sha = local.dir_sha
+  }
 
 }
