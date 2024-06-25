38fc9f43952a0832c9a2bbb7206789b778bb2c54
sh1un <a0923183408@gmail.com>
2024-06-26 07:30:27 +0800

    test: CI/CD test

--- practice/api_gateway_to_lambda_docker_to_dynamodb/lambda_function.py
+++ practice/api_gateway_to_lambda_docker_to_dynamodb/lambda_function.py
@@ -16,6 +16,7 @@ def lambda_handler(event, context):
     try:
         response = table.scan()
         items = response.get("Items", [])
+        print("test")
 
         logger.info("Scan succeeded: %s", json.dumps(items))
 
